3ee4a48ae3678afcc01637a61b26ae7095285d2b
leonimoy <leonard8nimoy7@gmail.com>
2023-01-16 17:54:56 +0900

    20230116 password encoding 을 위해서          com.global.config.AppConfig 클래스에          public PasswordEncoder passwordEncoder() 작성          AccountService 클래스에          private final PasswordEncoder passwordEncoder 추가하고          saveNewAccount() 메소드에          .password(passwordEncoder.encode(signUpForm.getPassword())) 작성          AccountControllerTest 에서          signUpSubmit_with_correct_input() 메소드에          assertNotNull(account);          assertNotEquals(account.getPassword(), "12345678");          추가하고 test 함

--- src/main/java/com/global/account/AccountRepository.java
+++ src/main/java/com/global/account/AccountRepository.java
@@ -8,4 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 public interface AccountRepository extends JpaRepository<Account, Long> {
   boolean existsByEmail(String email);
   boolean existsByNickName(String nickname);
+
+  Account findByEmail(String email);
+
 }
--- src/main/java/com/global/account/AccountService.java
+++ src/main/java/com/global/account/AccountService.java
@@ -4,6 +4,7 @@ import com.global.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 public class AccountService {
   private final AccountRepository accountRepository;
   private final JavaMailSender javaMailSender;
+  private final PasswordEncoder passwordEncoder;
+
 
   public void processNewAccount(SignUpForm signUpForm) {
     Account newAccount = saveNewAccount(signUpForm);
@@ -23,7 +26,7 @@ public class AccountService {
     Account account = Account.builder()
       .email(signUpForm.getEmail())
       .nickName(signUpForm.getNickName())
-      .password(signUpForm.getPassword())
+      .password(passwordEncoder.encode(signUpForm.getPassword()))
       .studyCreateByWeb(true)
       .studyEnrollmentResultByWeb(true)
       .studyUpdateByWeb(true)
