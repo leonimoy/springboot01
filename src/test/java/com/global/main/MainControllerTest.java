9efc5160415fe3cdc397939b9bcf3d163352ea95
leonimoy <leonard8nimoy7@gmail.com>
2023-01-19 11:13:27 +0900

    20230119
    [로그인, 로그아웃 테스트하기]
    
    void login_with_email() 복사해서
    void login_with_nickName() 작성하고
    
    JUnit5 의 @BeforeEach 와 @AfterEach 작성함
    
    void login_with_email(), void login_with_nickName() 에서
    @BeforeEach 에 작성한 부분은 삭제함
    
    void login_with_email() 에서는
    .param("username", "globaltest@gmail.com")
    로 설정하고
    
    void login_with_nickName() 에서는
    .param("username", "globaltest")
    로 설정함
    
    MainControllerTest 클래스 전체를 실행함
    
    테스트 성공

--- src/test/java/com/global/main/MainControllerTest.java
+++ src/test/java/com/global/main/MainControllerTest.java
@@ -31,10 +31,16 @@ class MainControllerTest {
   @Autowired
   AccountService accountService;
 
-  @DisplayName("이메일로 로그인 성공 테스트")
-  @Test
-  void login_with_email() throws Exception{
+  // AccountRepository 에 있는 method 를
+  // 사용하기 위해서 의존성 주입 받음
+  @Autowired
+  AccountRepository accountRepository;
 
+  // @BeforeEach [test1] @AfterEach @BeforeEach [test2] @AfterEach @BeforeEach [test3] @AfterEach
+  // @BeforeEach : 모든 test 를 실행할 때마다
+  //               먼저 실행되는 부분을 의미함
+  @BeforeEach
+  void beforeEach(){
     // 아래의 정보에 해당하는 account 계정이 생성되고 저장됨
     // 이메일도 전송함
     SignUpForm signUpForm = new SignUpForm();
@@ -42,6 +48,27 @@ class MainControllerTest {
     signUpForm.setEmail("globaltest@gmail.com");
     signUpForm.setPassword("12345678");
     accountService.processNewAccount(signUpForm);
+  }
+
+  // @AfterEach : 모든 test 를 실행할 때마다
+  //              나중에 실행되는 부분을 의미함
+  // test 를 두 개 이상 실행할 때
+  // @BeforeEach 에서 DB 에 넣은 내용이 중복되어서
+  // 들어가게 되므로 @AfterEach 에서 삭제해 주어야 함
+  //  ㄴ 삭제하려면 AccountRepository 가 필요함
+  //      ㄴ  멤버변수에서 @Autowired 해 줌
+  @AfterEach
+  void afterEach(){
+    // @BeforeEach 에서 DB 에 넣은 내용을 삭제함
+    accountRepository.deleteAll();
+  }
+
+
+  @DisplayName("이메일로 로그인 성공 테스트")
+  @Test
+  void login_with_email() throws Exception{
+
+    /*  @BeforeEach 에서 실행하는 부분 */
 
     // post 방식으로 요청(/login)을 보내면
     // Spring Security 가 로그인 처리를 해 줌
@@ -64,6 +91,34 @@ class MainControllerTest {
            .andExpect(authenticated().withUsername("globaltest"));
   }
 
+  @DisplayName("nickName 으로 로그인 성공 테스트")
+  @Test
+  void login_with_nickName() throws Exception{
+
+    /*  @BeforeEach 에서 실행하는 부분 */
+
+    // post 방식으로 요청(/login)을 보내면
+    // Spring Security 가 로그인 처리를 해 줌
+    // 이메일로 로그인하려고
+    // param("username", "globaltest@gmail.com") 설정함
+    // Spring Security 를 사용하면, csrf 라는 protection 이 활성화되어 있음
+    //  .with(csrf())  <-- csrf token 을 같이 전송함
+    // .andExpect(authenticated().withUsername("globaltest"))
+    //   ㄴ globaltest 라는 username(nickName) 으로 인증됨
+    // 이메일이 아니라, nickName으로 인증됨
+    //  ㄴ UserAccount 클래스의 생성자에서
+    //     super(account.getNickName(),  <-- 이렇게 설정했으므로...
+    //             ㄴ username 부분을 nickName 으로 반환했음
+    mockMvc.perform(post("/login")
+        .param("username", "globaltest")
+        .param("password", "12345678")
+        .with(csrf()))
+      .andExpect(status().is3xxRedirection())
+      .andExpect(redirectedUrl("/"))
+      .andExpect(authenticated().withUsername("globaltest"));
+
+
+  }
 
 
 
